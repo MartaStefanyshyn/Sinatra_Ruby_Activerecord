503c2e2ceb29f2bd922adf2b51e5bc59faf47d75
Marta Stefanyshyn <martaolehivna@gmail.com>
2015-12-08 17:50:56 +0200

    Method convert without select

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -6,8 +6,13 @@ class Comment < ActiveRecord::Base
 
   def self.convert(comments, input_id)
     result = []
+    query_results = []
 
-    query_results = comments.select(:id, :content, :note_id, :parent_id).where(parent_id: input_id)
+    comments.each do |comment|
+      if com.parent_id == input_id
+        query_results << com
+      end
+    end
     query_results.each do |element|
       result.push({'id'=>element[:id],'content'=>element[:content],'note_id'=>element[:note_id],'parent_id'=>element[:parent_id],'subcomment'=>self.convert(comments, element[:id])})
     end
